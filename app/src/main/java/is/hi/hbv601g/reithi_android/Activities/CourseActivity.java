6bb8fcada139d188ae5ce9d483265fb514547ada
JonasHakon <jonashakon01@gmail.com>
2023-03-02 18:58:30 +0000

    Byrja að hanna course activity

--- app/src/main/java/is/hi/hbv601g/reithi_android/Activities/CourseActivity.java
+++ app/src/main/java/is/hi/hbv601g/reithi_android/Activities/CourseActivity.java
@@ -1,4 +1,21 @@
 package is.hi.hbv601g.reithi_android.Activities;
 
-public class CourseActivity  {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import is.hi.hbv601g.reithi_android.NetworkManager;
+import is.hi.hbv601g.reithi_android.R;
+
+public class CourseActivity extends AppCompatActivity {
+
+    private static final String TAG = "coursePageActivity";
+
+    private NetworkManager mNetworkManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+    }
 }
